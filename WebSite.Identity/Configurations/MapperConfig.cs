edea3019dff8388734037a68ecfb36e05ccbf92d
Vlad <vladavramchuk@gmail.com>
2024-11-28 01:03:10 +0200

    Fix issue with mapping.

--- WebSite.Identity/Configurations/MapperConfig.cs
+++ WebSite.Identity/Configurations/MapperConfig.cs
@@ -9,7 +9,7 @@ namespace WebSite.Identity.Configurations
         public MapperConfig() 
         {
             CreateMap<RegisterJsonModel, User>()
-                .ForMember(to => to.UserName, from => from.MapFrom(src => src.FirstName))
+                .ForMember(to => to.UserName, from => from.MapFrom(src => src.Email))
                 .ForMember(to => to.Email, from => from.MapFrom(src => src.Email))
                 .ForMember(to => to.FirstName, from => from.MapFrom(src => src.FirstName))
                 .ForMember(to => to.LastName, from => from.MapFrom(src => src.LastName));
